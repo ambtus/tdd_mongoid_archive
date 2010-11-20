77983f14ee125ca45c2ecc8460f434df0df18501
ambtus <github@ambt.us>
2010-11-20 15:04:18 -0500

    changed name of application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Mongoid::Application.configure do
+TddMongoid::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
